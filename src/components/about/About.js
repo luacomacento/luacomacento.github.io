25b95f01b7c408c559399e725f2a523b43e0d628
Luá Octaviano <luacomacento@gmail.com>
2022-02-20 15:04:25 -0300

    feat: alteração do layout da seção #about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import SectionTitle from '../SectionTitle';
 import './About.css';
 
+
 class About extends Component {
   render() { 
     return (
       <section id="about">
         <SectionTitle text="Sobre mim" />
         <div className="about-container">
-          <img className="profile-pic" src="https://luacomacento.github.io/images/profile.jpg"></img>
-          <div>
+          <img className="profile-pic" src={'../../images/profile-tall.jpg'}></img>
+          <div class="about-text-container">
             <p>
-              Luá tem 24 anos, é moradora de Belo Horizonte (mesmo sendo carioca da gema), vascaína e apaixonada por tecnologia. Quando criança, morava numa casa cor-de-casa-de-vó no subúrbio do Rio de Janeiro e acredita que isso diz muito sobre sua personalidade.
+              <b>Luá</b> tem 24 anos, é moradora de Belo Horizonte (mesmo sendo carioca da gema), vascaína e apaixonada por tecnologia. Quando criança, morava numa casa cor-de-casa-de-vó no subúrbio do Rio de Janeiro e acredita que isso diz muito sobre sua personalidade.
             </p>
             <h3>Algumas tecnologias que conheço:</h3>
             <div class="about-skills-list">
