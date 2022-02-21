d77769d044c238af3cbe91bf92dbabd9e18e2a4f
Luá Octaviano <luacomacento@gmail.com>
2022-02-21 12:27:41 -0300

    fix: arruma um problema em que os badges do ProjectCard apareciam apenas para o primeiro card

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -13,15 +13,18 @@ class ProjectCard extends Component {
     this.toggleBadges = this.toggleBadges.bind(this);
   }
 
-  toggleBadges() {
-    const toolsBadgeContainer = document.querySelector('.project-tools-container')
-
-    if (!this.state.wasMouseOver) {
-      toolsBadgeContainer.style.opacity = '1';
-      this.setState({ wasMouseOver: true })
-    } else {
-      toolsBadgeContainer.style.opacity = '0';
-      this.setState({ wasMouseOver: false })
+  toggleBadges(event) {
+    const toolsBadgeContainer = event.currentTarget.firstChild.firstChild;
+  
+    // Garante que o código só seja rodado caso exista algum badge no container.
+    if (toolsBadgeContainer.innerHTML) {
+      if (!this.state.wasMouseOver) {
+        toolsBadgeContainer.style.opacity = '1';
+        this.setState({ wasMouseOver: true })
+      } else {
+        toolsBadgeContainer.style.opacity = '0';
+        this.setState({ wasMouseOver: false })
+      }
     }
   }
 
@@ -33,7 +36,8 @@ class ProjectCard extends Component {
       .map((skill) => tools.find((tool) => tool.slug === skill));
 
     return (
-      <div className="project-card" onMouseEnter={this.toggleBadges} onMouseLeave={this.toggleBadges}>
+      <div className="project-card" onMouseEnter={this.toggleBadges}
+        onMouseLeave={this.toggleBadges}>
         <div className="project-img-container">
           <div className="project-tools-container">
             {skillsBadges.map((item) => {
