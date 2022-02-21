09c78bde0375b4700822a70a847211fa1a7176e9
Luá Octaviano <luacomacento@gmail.com>
2022-02-21 12:09:44 -0300

    feat: adicionadas imagens de projetos e novo projeto (todo-list)

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,24 @@
 const DEFAULT_GITHUB_URL = 'https://github.com/luacomacento';
-const DEFAULT_PREVIEW_URL = 'https://luacomacento.github.io'
+const DEFAULT_PREVIEW_URL = 'https://luacomacento.github.io';
+const IMAGES_URL = './images/previews';
 
 export const projects = [
   {
     title: 'Minesweeper',
-    image: 'https://github.com/luacomacento/minesweeper/raw/main/preview.gif',
-    description: 'A simple minesweeper game made with pure Javascript.',
+    image: `${IMAGES_URL}/minesweeper.gif`,
+    description: 'Um clássico jogo de Campo Minado feito em JavaScript puro.',
     skills: ['javascript', 'html5', 'css3'],
     githubLink: `${DEFAULT_GITHUB_URL}/minesweeper`,
     previewLink: `${DEFAULT_PREVIEW_URL}/minesweeper`,
   },
-  // {
-  //   title: 'To-Do List',
-  //   image: 'https://github.com/raphaelalmeidamartins/to-do-list/raw/main/preview.gif',
-  //   description: 'Lorem ipsum dolor sit amet consecteur adiplisci elit.',
-  //   skills: '',
-  //   githubLink: '',
-  //   previewLink: '',
-  // },
+  {
+    title: 'To-Do List',
+    image: `${IMAGES_URL}/todo-list.gif`,
+    description: 'Lorem ipsum dolor sit amet consecteur adiplisci elit.',
+    skills: [],
+    githubLink: '',
+    previewLink: '',
+  },
   // {
   //   title: 'Pixels Art',
   //   image: 'https://github.com/raphaelalmeidamartins/pixels-art/raw/main/preview.gif',
