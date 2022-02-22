cfa617ab5c560b58292f1aa99b19c2caec369817
Luá Octaviano <luacomacento@gmail.com>
2022-02-22 11:24:30 -0300

    feat: adicionadas novas informações e estilo no footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { FaEnvelope, FaGithubAlt, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 import './Footer.css';
 
 class Footer extends Component {
   render() { 
     return (
-      <section>
-        <footer>{"Desenvolvido em React por Luá :)"}</footer>
-      </section>
+      <>
+        <footer>
+          <div className="social-container">
+            <a className="social-icon" href="/">
+              <FaGithubAlt />
+            </a>
+            <a className="social-icon" href="/">
+              <FaLinkedinIn />
+            </a>
+            <a className="social-icon" href="/">
+              <FaTwitter />
+            </a>
+            <a className="social-icon" href="/">
+              <FaEnvelope />
+            </a>
+          </div>
+          
+          {"Design e código por Luá."}
+          <br></br>
+          {"Desenvolvido com ReactJS e noites mal dormidas. © 2022"}
+        </footer>
+      </>
     );
   }
 }
