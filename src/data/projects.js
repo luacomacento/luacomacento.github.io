295229c74ad18e054161da9ee4db221f4ea08bc8
Luá Octaviano <luacomacento@gmail.com>
2022-05-24 16:54:51 -0300

    feat: altera a imagem de perfil e prepara o card do app de receitas

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,9 +4,17 @@ const IMAGES_URL = './images/previews';
 
 export const projects = [
   {
+    title: 'Yummy App',
+    image: `${IMAGES_URL}/yummy.gif`,
+    description: 'Uma aplicação front-end completa de receitas, tanto para comidas quanto para bebidas. Permite ao usuário buscar, compartilhar e salvar as receitas, consumindo duas APIs públicas.',
+    skills: ['javascript', 'html5', 'css3'],
+    githubLink: `${DEFAULT_GITHUB_URL}/yummy-app`,
+    previewLink: `${DEFAULT_PREVIEW_URL}/yummy-app`,
+  },
+  {
     title: 'Minesweeper',
     image: `${IMAGES_URL}/minesweeper.gif`,
-    description: 'Um clássico jogo de Campo Minado feito em JavaScript puro.',
+    description: 'Um clássico jogo de Campo Minado feito em JavaScript puro. Possui três níveis de dificuldade, com posições aleatórias das bombas.',
     skills: ['javascript', 'html5', 'css3'],
     githubLink: `${DEFAULT_GITHUB_URL}/minesweeper`,
     previewLink: `${DEFAULT_PREVIEW_URL}/minesweeper`,
@@ -22,7 +30,7 @@ export const projects = [
   {
     title: 'To-Do List',
     image: `${IMAGES_URL}/todo-list.gif`,
-    description: 'Uma lista de tarefas simples, usando localStorage para salvar os itens. Desenvolvido como projeto do curso da Trybe.',
+    description: 'Uma lista de tarefas básica, usando localStorage para salvar os itens. Desenvolvido como projeto do curso da Trybe.',
     skills: ['javascript', 'html5', 'css3'],
     githubLink: `${DEFAULT_GITHUB_URL}/todo-list`,
     previewLink: `${DEFAULT_PREVIEW_URL}/todo-list`,
@@ -38,7 +46,7 @@ export const projects = [
   {
     title: 'Pixels Art',
     image: `${IMAGES_URL}/pixels-art.gif`,
-    description: 'Um simples quadro de pixels que permite ao usuário pintar ponto a ponto em cores de sua escolha (com opções geradas aleatoriamente pela página). Desenvolvido como projeto do curso da Trybe.',
+    description: 'Um quadro de pixels que permite ao usuário pintar ponto a ponto em cores de sua escolha (com opções geradas aleatoriamente pela página). Desenvolvido como projeto do curso da Trybe.',
     skills: ['javascript', 'html5', 'css3'],
     githubLink: `${DEFAULT_GITHUB_URL}/pixels-art`,
     previewLink: `${DEFAULT_PREVIEW_URL}/pixels-art`,
